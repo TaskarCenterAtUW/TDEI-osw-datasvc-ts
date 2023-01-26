df5ac94aebdc2df75e0bc18d2c0bcc981308cdad
Mahesh Badmanji <mahesh_badmanji@yahoo.com>
2023-01-26 17:54:22 +0530

    OSW migration from ORM to Native SQL

--- src/database/entity/osw-version-entity.ts
+++ src/database/entity/osw-version-entity.ts
@@ -1,60 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 import { IsNotEmpty } from 'class-validator';
-import { Polygon } from "../../model/polygon-model";
+import { BaseDto } from '../../model/base-dto';
 
-@Entity()
-export class OswVersions {
+export class OswVersions extends BaseDto {
 
-    @PrimaryGeneratedColumn()
     id!: number;
-
-    @Column()
     @IsNotEmpty()
     tdei_record_id: string = "";
-
-    @Column({ nullable: true, default: 0 })
     confidence_level: number = 0;
-
-    @Column()
     @IsNotEmpty()
     tdei_org_id: string = "";
-
-    @Column()
     @IsNotEmpty()
     file_upload_path: string = "";
-
-    @Column()
     @IsNotEmpty()
     uploaded_by: string = "";
-
-    @Column()
     @IsNotEmpty()
     collected_by: string = "";
-
-    @Column("timestamp")
     @IsNotEmpty()
     collection_date: Date = new Date();
-
-    @Column()
     @IsNotEmpty()
     collection_method: string = "";
-
-    @Column("timestamp")
     @IsNotEmpty()
     valid_from: Date = new Date();
-
-    @Column("timestamp")
     @IsNotEmpty()
     valid_to: Date = new Date();
-
-    @Column()
     @IsNotEmpty()
     data_source: string = "";
-
-    @Column()
     @IsNotEmpty()
     osw_schema_version: string = "";
+    polygon: any = {};
 
-    @Column("json")
-    polygon: Polygon = new Polygon();
+    constructor(init?: Partial<OswVersions>) {
+        super();
+        Object.assign(this, init);
+    }
 }
