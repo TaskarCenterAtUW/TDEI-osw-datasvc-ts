f3c6e9c67fa5ee155c55de130852a09cff529b28
Achyut Kumar M <achyutkumar.m@gmail.com>
2023-09-21 00:20:51 +0530

    add oswPayload

--- test/common/tdei-object-faker.ts
+++ test/common/tdei-object-faker.ts
@@ -111,6 +111,56 @@ export class TdeiObjectFaker {
         return oswValidationSuccessMessage;
     }
 
+    static getOswPayload2(){
+        return {
+            "tdei_org_id": "e1956869-02d9-4e14-8391-6024406ced41",
+            "collected_by": "testuser",
+            "collection_date": "2023-03-02T04:22:42.493Z",
+            "collection_method": "manual",
+            "publication_date": "2023-03-02T04:22:42.493Z",
+            "data_source": "TDEITools",
+            "polygon": {
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "coordinates": [
+                  [
+                    [
+                      -122.32615394375401,
+                      47.61267259760652
+                    ],
+                    [
+                      -122.32615394375401,
+                      47.60504395643625
+                    ],
+                    [
+                      -122.3155850364906,
+                      47.60504395643625
+                    ],
+                    [
+                      -122.3155850364906,
+                      47.61267259760652
+                    ],
+                    [
+                      -122.32615394375401,
+                      47.61267259760652
+                    ]
+                  ]
+                ],
+                "type": "Polygon"
+              }
+            }
+          ]
+        },
+            "osw_schema_version": "v2.0"
+          }
+    }
+
+
+
 
     static getMockUploadFile() {
         return {
