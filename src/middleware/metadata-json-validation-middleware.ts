355b08632a9cbd694507546009ca46fe944763c3
MashB <mahesh_badmanji@yahoo.com>
2024-01-12 12:48:01 +0530

    metadata validation implementation

--- src/middleware/metadata-json-validation-middleware.ts
+++ src/middleware/metadata-json-validation-middleware.ts
@@ -5,6 +5,11 @@
 
 import { NextFunction, Request, Response } from "express";
 import { InputException } from "../exceptions/http/http-exceptions";
+import Ajv, { ErrorObject } from "ajv";
+import metaschema from "../../schema/osw-metadata.schema.json";
+
+const ajv = new Ajv({ allErrors: true });
+const validate = ajv.compile(metaschema);
 
 export async function metajsonValidator(req: Request, res: Response, next: NextFunction) {
 
@@ -15,6 +20,12 @@ export async function metajsonValidator(req: Request, res: Response, next: NextF
         }
 
         const metadata = JSON.parse(metadataFile[0].buffer);
+        const valid = validate(metadata);
+        if (!valid) {
+            const message = validate.errors?.map((error: ErrorObject) => error.message).join(", \n");
+            console.error("Metadata json validation error : ", message);
+            next(new InputException(message as string));
+        }
         next();
     }
     catch (error) {
