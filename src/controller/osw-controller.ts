920082d15dd1852f9ef3cab41e08078ee1f67c8e
Naresh Kumar D <susrisha@gmail.com>
2024-04-05 11:35:37 +0530

    fixes the version urls
    Fixes version urls

--- src/controller/osw-controller.ts
+++ src/controller/osw-controller.ts
@@ -100,9 +100,9 @@ class GtfsOSWController implements IController {
 
     getVersions = async (request: Request, response: express.Response, next: NextFunction) => {
         let versionsList = new Versions([{
-            documentation: environment.gatewayUrl as string,
-            specification: "https://github.com/OpenSidewalks/OpenSidewalks-Schema",
-            version: "v0.1"
+            documentation: environment.schemaDocumentationUrl as string ?? '',
+            specification: environment.schemaUrl as string ?? '',
+            version: "0.2"
         }]);
 
         response.status(200).send(versionsList);
