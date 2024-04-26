d6c27f85ca6395abb28d7ed3f42a61430784e71f
MashB <mahesh_badmanji@yahoo.com>
2024-04-26 11:01:44 +0530

    moving update before job

--- src/orchestrator/workflows-handlers/pathways/upload/upload-pathways-validation-response-handler.ts
+++ src/orchestrator/workflows-handlers/pathways/upload/upload-pathways-validation-response-handler.ts
@@ -2,11 +2,12 @@ import EventEmitter from "events";
 import { WorkflowHandlerBase } from "../../../models/orchestrator-base-model";
 import { IOrchestratorService } from "../../../services/orchestrator-service";
 import { QueueMessage } from "nodets-ms-core/lib/core/queue";
-import { UpdateJobDTO } from "../../../../model/job-dto";
+import { JobDTO, UpdateJobDTO } from "../../../../model/job-dto";
 import { JobStatus } from "../../../../model/jobs-get-query-params";
 import jobService from "../../../../service/job-service";
 import dbClient from "../../../../database/data-source";
 import { DatasetEntity } from "../../../../database/entity/dataset-entity";
+import { JobEntity } from "../../../../database/entity/job-entity";
 import { RecordStatus } from "../../../../model/dataset-get-query-params";
 
 export class PathwaysUploadValidationResponseHandler extends WorkflowHandlerBase {
@@ -24,15 +25,18 @@ export class PathwaysUploadValidationResponseHandler extends WorkflowHandlerBase
     async handleRequest(message: QueueMessage, delegate_worflow: string[], params: any): Promise<void> {
         console.log(`Triggered ${this.eventName} :`, message.messageType);
         try {
+            const result = await dbClient.query(JobEntity.getJobByIdQuery(message.messageId));
+            const job = JobDTO.from(result.rows[0]);
+
+            await dbClient.query(DatasetEntity.getStatusUpdateQuery(job.response_props.tdei_dataset_id, RecordStatus["Pre-Release"]));
+
             let updateJobDTO = UpdateJobDTO.from({
                 job_id: message.messageId,
                 message: message.data.message,
                 status: message.data.success ? JobStatus.COMPLETED : JobStatus.FAILED,
                 response_props: {}
             })
-            let job = await jobService.updateJob(updateJobDTO);
-            await dbClient.query(DatasetEntity.getStatusUpdateQuery(job.response_props.tdei_dataset_id, RecordStatus["Pre-Release"]));
-
+            await jobService.updateJob(updateJobDTO);
             this.delegateWorkflowIfAny(delegate_worflow, message);
 
         } catch (error) {
