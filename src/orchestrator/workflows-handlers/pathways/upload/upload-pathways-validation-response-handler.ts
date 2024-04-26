d4f8623f764f9a751ba8311bb7e2856ce8405630
MashB <mahesh_badmanji@yahoo.com>
2024-04-26 10:52:51 +0530

    bug fix pre-reelase after upload

--- src/orchestrator/workflows-handlers/pathways/upload/upload-pathways-validation-response-handler.ts
+++ src/orchestrator/workflows-handlers/pathways/upload/upload-pathways-validation-response-handler.ts
@@ -5,6 +5,9 @@ import { QueueMessage } from "nodets-ms-core/lib/core/queue";
 import { UpdateJobDTO } from "../../../../model/job-dto";
 import { JobStatus } from "../../../../model/jobs-get-query-params";
 import jobService from "../../../../service/job-service";
+import dbClient from "../../../../database/data-source";
+import { DatasetEntity } from "../../../../database/entity/dataset-entity";
+import { RecordStatus } from "../../../../model/dataset-get-query-params";
 
 export class PathwaysUploadValidationResponseHandler extends WorkflowHandlerBase {
 
@@ -27,7 +30,9 @@ export class PathwaysUploadValidationResponseHandler extends WorkflowHandlerBase
                 status: message.data.success ? JobStatus.COMPLETED : JobStatus.FAILED,
                 response_props: {}
             })
-            await jobService.updateJob(updateJobDTO);
+            let job = await jobService.updateJob(updateJobDTO);
+            await dbClient.query(DatasetEntity.getStatusUpdateQuery(job.response_props.tdei_dataset_id, RecordStatus["Pre-Release"]));
+
             this.delegateWorkflowIfAny(delegate_worflow, message);
 
         } catch (error) {
