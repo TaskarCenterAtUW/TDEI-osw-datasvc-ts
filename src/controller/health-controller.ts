cb8b21561ef2bfbb11388471fd8c8d0cdca9ba84
Naresh Kumar D <susrisha@gmail.com>
2024-05-07 15:37:14 +0530

    Update health-controller.ts
    Updated health controller for testing

--- src/controller/health-controller.ts
+++ src/controller/health-controller.ts
@@ -1,5 +1,9 @@
 import express, { Request } from "express";
 import { IController } from "./interface/IController";
+import { Core } from "nodets-ms-core";
+// import storageService from "../service/storage-service";
+import path from "path";
+import fs from "fs";
 
 class HealthController implements IController {
     public path = '/health';
@@ -11,12 +15,48 @@ class HealthController implements IController {
 
     public intializeRoutes() {
         this.router.get(`${this.path}/ping`, this.getping);
+        this.router.get(`${this.path}/test-upload-zip-stream`, this.testUploadZipStream)
+        this.router.get(`${this.path}/test-redirect`, this.testRedirect)
+        this.router.get(`${this.path}/test-local-file`, this.testLocalFile)
     }
 
     public getping = async (request: Request, response: express.Response) => {
         // return loaded posts
         response.status(200).send("I'm healthy !!");
     }
+
+    testUploadZipStream = async (request: Request, response: express.Response) => {
+        // File Url is https://tdeisamplestorage.blob.core.windows.net/osw/test_upload/seattle.zip
+        const storageClient = Core.getStorageClient();
+        const containerName = "osw";
+        const blobName = "test_upload/seattle.zip";
+        const theFile = await storageClient?.getFile(containerName, blobName);
+        const fileStream = await theFile?.getStream()
+        // Respond with the file stream
+        response.setHeader('Content-Type', 'application/zip');
+        response.setHeader('Content-Disposition', `attachment; filename=seattle.zip`);
+        // response.send(fileStream)
+        // stream the content from fileStream to response
+        fileStream?.pipe(response);
+        
+    }
+
+    testRedirect = async (request: Request, response: express.Response) => {
+        // Redirect the response to a different url
+        response.redirect(301,'https://tdeisamplestorage.blob.core.windows.net/osw/test_upload/seattle.zip');
+    }
+
+    testLocalFile = async (request: Request, response: express.Response) => {
+        // get the path to osw-output folder
+        const filePath = path.resolve(__dirname, '../../../osw-output', 'seattle.zip');
+        // Respond with the file stream
+        // response.setHeader('Content-Type', 'application/zip');
+        // response.setHeader('Content-Disposition', `attachment; filename=seattle.zip`);
+        // const stream = fs.createReadStream(filePath);
+        // stream.pipe(response);
+        response.sendFile(filePath);
+    }
+
 }
 
 const healthController = new HealthController();
