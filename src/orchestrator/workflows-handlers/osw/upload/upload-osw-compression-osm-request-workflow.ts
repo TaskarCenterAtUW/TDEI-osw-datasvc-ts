0a6485ff4d7540d7b8a60089618197f5702a8b02
Naresh Kumar D <susrisha@gmail.com>
2024-05-27 12:51:32 +0530

    added dataset osm and osw download url flows
    - Added dataset OSM and OSW zipping components

--- src/orchestrator/workflows-handlers/osw/upload/upload-osw-compression-osm-request-workflow.ts
+++ src/orchestrator/workflows-handlers/osw/upload/upload-osw-compression-osm-request-workflow.ts
@@ -9,10 +9,10 @@ import { JobEntity } from "../../../../database/entity/job-entity";
 import { JobDTO } from "../../../../model/job-dto";
 import path from "path";
 
-export class UploadCompressionRequestWorkflow extends WorkflowBase {
+export class UploadCompressionOSMRequestWorkflow extends WorkflowBase {
 
     constructor(workflowEvent: EventEmitter, orchestratorServiceInstance: IOrchestratorService) {
-        super(workflowEvent, orchestratorServiceInstance, "OSW_UPLOAD_COMPRESSION_REQUEST_WORKFLOW");
+        super(workflowEvent, orchestratorServiceInstance, "OSW_UPLOAD_COMPRESSION_OSM_REQUEST_WORKFLOW");
     }
 
     async handleWorkflow(message: QueueMessage, params: any): Promise<void> {
@@ -24,20 +24,20 @@ export class UploadCompressionRequestWorkflow extends WorkflowBase {
             const job = JobDTO.from(result.rows[0]);
             // Get the dataset details
             const dataset = await tdeiCoreService.getDatasetDetailsById(job.response_props.tdei_dataset_id);
-            let input_urls = [dataset.latest_dataset_url, dataset.metadata_url];
+            let input_urls = [dataset.latest_osm_url, dataset.metadata_url];
             if (dataset.changeset_url && dataset.changeset_url != "" && dataset.changeset_url != null) {
                 input_urls.push(dataset.changeset_url);
             }
             let directory = path.dirname(dataset.latest_dataset_url);
             console.log('directory ',directory);
-            let output_path = directory + '/zip/'+dataset.tdei_dataset_id+'.zip';
+            let output_path = directory + '/zip/'+dataset.tdei_dataset_id+'.osm.zip';
             console.log('output path ',output_path);
             //Compose the meessage
             let queueMessage = QueueMessage.from({
                 messageId: message.messageId,
                 messageType: `${this.eventName}`, //will be set by the publish handler with params defined in config
                 data: {
-                    isOsm: false,
+                    isOsm: true,
                     input_urls: input_urls, // osw.zip, metada.json, 
                     output_path: output_path // <>/zip/dataset_id.zip
                 }
