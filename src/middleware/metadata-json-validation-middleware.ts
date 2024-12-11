0a14a9ce93b63892edc57c618540ed23f037ff39
MashB <mahesh_badmanji@yahoo.com>
2024-12-11 13:02:17 +0530

    metadata validation fix

--- src/middleware/metadata-json-validation-middleware.ts
+++ src/middleware/metadata-json-validation-middleware.ts
@@ -22,12 +22,16 @@ export async function metajsonValidator(req: Request, res: Response, next: NextF
         const metadata = JSON.parse(metadataFile[0].buffer);
         const valid = validate(metadata);
         if (!valid) {
-            let requiredMsg = validate.errors?.filter(z => z.keyword == "required").map((error: ErrorObject) => `${error.instancePath} : ${error.params.missingProperty}`).join(`, \n`);
+            let requiredPropertyMsg = validate.errors?.filter(z => z.keyword == "required").map((error: ErrorObject) => `${error.instancePath.replace(/^\//, '').replace('/', ' -> ')} : ${error.params.missingProperty}`).join(`, \n`);
+            let requiredEnumMsg = validate.errors?.filter(z => z.keyword == "enum").map((error: ErrorObject) => `${error.instancePath.replace(/^\//, '').replace('/', ' -> ')} : Allowed values ${error.params.allowedValues}`).join(`, \n`);
+            let invalidTypeMsg = validate.errors?.filter(z => z.keyword == "type" && z.params.type != 'null').map((error: ErrorObject) => `${error.instancePath.replace(/^\//, '').replace('/', ' -> ')} : Must be of type ${error.params.type}`).join(`, \n`);
             let additionalMsg = validate.errors?.filter(z => z.keyword == "additionalProperties").map((error: ErrorObject) => `${error.params.additionalProperty}`).join(`, \n`);
-            requiredMsg = requiredMsg != "" ? `Missing required properties : \n ${requiredMsg} ` : "";
-            additionalMsg = additionalMsg != "" ? `\n Additional properties found : \n ${additionalMsg} not allowed` : "";
-            console.error("Metadata json validation error : ", additionalMsg, requiredMsg);
-            next(new InputException((requiredMsg + "\n" + additionalMsg) as string));
+            requiredPropertyMsg = requiredPropertyMsg != "" ? `\nMissing required properties : \n ${requiredPropertyMsg} ` : "";
+            requiredEnumMsg = requiredEnumMsg != "" ? `\nInvalid property value : \n ${requiredEnumMsg} ` : "";
+            invalidTypeMsg = invalidTypeMsg != "" ? `\nInvalid property type : \n ${invalidTypeMsg} ` : "";
+            additionalMsg = additionalMsg != "" ? `\nAdditional properties found : \n ${additionalMsg} not allowed` : "";
+            console.error("Metadata json validation error : \n", additionalMsg, requiredPropertyMsg, requiredEnumMsg, invalidTypeMsg);
+            next(new InputException(("\n" + requiredPropertyMsg + "\n" + requiredEnumMsg + "\n" + invalidTypeMsg + "\n" + additionalMsg) as string));
         }
         else
             next();
