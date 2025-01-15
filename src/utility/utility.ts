ea1c76307de5bdd69e059db1748622eda310c958
sujata-m <sujatam@gaussiansolutions.com>
2025-01-15 13:27:17 +0530

    Added comments

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -113,19 +113,21 @@ export class Utility {
     }
 
     static calculateTotalSize(files: Express.Multer.File[]): number {
+        // Calculate the total size of the files inside the uploaded ZIP
         return files.reduce((total, file) => {
-          const zip = new AdmZip(file.buffer);
-          const zipEntries = zip.getEntries();
-
-          let fileSize = 0;
-          zipEntries.forEach((entry) => {
-              if (!entry.isDirectory) {
-                  fileSize += entry.header.size;
-              }
-          });
-
-          return total + fileSize;
-      }, 0);
+            const zip = new AdmZip(file.buffer);
+            // Get the entries of the zip file
+            const zipEntries = zip.getEntries();
+            let fileSize = 0;
+            zipEntries.forEach((entry) => {
+                // Check if the entry is a file and not a directory
+                if (!entry.isDirectory) {
+                    // Add the size of the file to the total size
+                    fileSize += entry.header.size;
+                }
+            });
+            return total + fileSize;
+        }, 0);
     }
 }
 
